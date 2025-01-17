4ae3970baa9c882e0b912f015b86e23df4e8055b
emingit4 <coniuk7@gmail.com>
2025-01-18 01:10:38 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pyrogram import Client, filters
 from pytgcalls import PyTgCalls
-from pytgcalls.types.input_stream import AudioPiped
+from pytgcalls.types import AudioPiped
 from youtube_search import YoutubeSearch
 import yt_dlp
 import logging
