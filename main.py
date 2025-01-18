8d79e7be1a83472b7d82f252f73352eb096e6680
emingit4 <coniuk7@gmail.com>
2025-01-18 04:58:54 +0400

    Update main.py

--- main.py
+++ main.py
@@ -57,6 +57,7 @@ async def play(_, message):
         "outtmpl": "downloads/%(title)s.%(ext)s",
         "quiet": True,
         "nocheckcertificate": True,  # Sertifikat yoxlanışını keçmək üçün əlavə edildi
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"  # Daha uzun user-agent əlavə edildi
     }
     try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
